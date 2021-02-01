019b9abb0c2f2b39785f921831120f25c451614c
rvasquez <rvasquez1@gmail.com>
2021-02-01 18:55:31 -0500

    adding jaws

--- config/connection.js
+++ config/connection.js
@@ -16,13 +16,7 @@ else {
 });
 }
 // Make connection.
-connection.connect((err) => {
-  if (err) {
-    console.error(`error connecting: ${err.stack}`);
-    return;
-  }
-  console.log(`connected as id ${connection.threadId}`);
-});
+connection.connect();
 
 // Export connection for our ORM to use.
 module.exports = connection;
