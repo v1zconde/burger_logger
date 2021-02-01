7f5a87aa0345f6e01ec114cc5323a754a6f11520
rvasquez <rvasquez1@gmail.com>
2021-02-01 18:22:42 -0500

    test connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
+var connection;
 
 if (process.env.JAWSDB_URL){
 connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
 else {
-const connection = mysql.createConnection({
+  connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
